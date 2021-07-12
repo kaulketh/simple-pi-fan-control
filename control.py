7c859c7681aeca5f2ecee12f4f9e8973af270bff
Thomas <kaulketh@gmail.com>
2021-07-12 11:17:16 +0200

    minor changes

--- control.py
+++ control.py
@@ -34,18 +34,18 @@ class FanControl:
         """
         GPIO.setup(self.__pin, GPIO.OUT)
         r = GPIO.input(self.__pin)
-        self.__out.write(f"### Fan pin {self.__pin}, state={r}\n")
+        self.__out.write(f"<<< Fan pin {self.__pin}, state={r}\n")
         return int(r)
 
     def __thermal_zone_temp(self) -> float:
         with open(FanControl.__THERMALZONE, "r") as file:
             result = float(file.read())
-        self.__out.write(f"### {FanControl.__THERMALZONE} >> {result}\n")
+        self.__out.write(f"<<< {FanControl.__THERMALZONE} >> {result}\n")
         return result / 1_000
 
     def __vcgencmd_temp(self) -> float:
         result = os.popen(FanControl.__VCGENCMD).readline()
-        self.__out.write(f"### {FanControl.__VCGENCMD} >> {result}")
+        self.__out.write(f"<<< {FanControl.__VCGENCMD} >> {result}")
         return float(result.replace("temp=", "").replace("'C\n", ""))
 
     def __run(self):
@@ -59,13 +59,13 @@ class FanControl:
 
                 if temp >= self.__max and self.__pin_state() == 0:
                     self.__out.write(
-                        f"### "
+                        f">>> "
                         f"{temp1}°C and {temp2}°C >= max limit, "
                         f"fan on.\n")
                     GPIO.output(self.__pin, True)
                 if temp <= self.__min and self.__pin_state() == 1:
                     self.__out.write(
-                        f"### "
+                        f">>> "
                         f"{temp1}°C and {temp2}°C , temperature ok, "
                         f"fan off.\n")
                     GPIO.output(self.__pin, False)
